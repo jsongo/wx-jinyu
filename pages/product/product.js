7da9d3b2e47e48fe96d1771c5bbe81138b926c14
jason <jsongo@qq.com>
2016-11-08 11:22:17 +0800

    产品详情页ok了

--- pages/product/product.js
+++ pages/product/product.js
@@ -4,9 +4,20 @@ var ProductCtl = require('../../handler/productCtl');
 var app = getApp();
 Page({
     data: {
+        winH: 0,
+        descImgHeight: 0,
         product: []
     },
+    winW: 0,
     onLoad: function(opt) {
+        wx.getSystemInfo({
+            success: (res)=>{
+                this.setData({
+                    winH: res.windowHeight
+                });
+                this.winW = res.windowWidth;
+            }
+        });
         let productCtl = new ProductCtl();
         let id = opt && opt.id || 0;
         if (id) {
@@ -14,10 +25,25 @@ Page({
                 wx.setNavigationBarTitle({
                     title: data.name
                 });
+                let gallery = data.gallery;
+                let desc = data.desc;
+                let m = desc.match(/(\/[^&]+)&/);
+                if (m.length > 1) {
+                    data.descImg = m[1];
+                }
+                data.gallery = gallery.split(',');
                 this.setData({
                     product: data
                 });
             });
         }
+    },
+    descImgLoaded: function(event) {
+        var width = event.detail.width,
+            height = event.detail.height,
+            descImgHeight = height*1.0/width*this.winH;
+        this.setData({
+            descImgHeight: parseInt(descImgHeight)
+        })
     }
 });
